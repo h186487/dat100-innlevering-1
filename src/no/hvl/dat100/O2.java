bc639a098f52cfff9727e1651638f12726f27f62
h186487 <nadia.lambrecht@gmail.com>
2024-09-11 15:07:46 +0200

    O2 and O3

--- src/no/hvl/dat100/O2.java
+++ src/no/hvl/dat100/O2.java
@@ -1,10 +1,46 @@
 package no.hvl.dat100;
 
+import javax.swing.JOptionPane;
+
 public class O2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+//		Input gross income from user using JOptionPane
+		String input = JOptionPane.showInputDialog("Enter your gross income");
+//		Parse the input string into an integer
+		int grossIncome = Integer.parseInt(input);
+		
+//		Define variable
+		double sumTax = 0.0;
+		
+//		"""Calculate tax for each bracket"""
+		if (grossIncome > 208050) {
+			//Bracket A: income between 208,051 - 292,850 with a 1,7% tax
+			sumTax += (Math.min(grossIncome, 292850) - 208051) * 0.017;
+		}
+		
+		if (grossIncome > 292850) {
+			//Bracket B: income between 292,851 - 670,000 with a 4,0% tax
+			sumTax += (Math.min(grossIncome,  670000) - 292851) * 0.04; 
+		}
+		
+		if (grossIncome > 670000) {
+			//Bracket C: income between 670,001 - 937,900 with a 13,6% tax
+			sumTax += (Math.min(grossIncome,  937900) - 670001) * 0.136;
+		}
+		
+		if (grossIncome > 937900) {
+			//Bracket D: income between 937,901 - 1,350,000 with a 16,6% tax
+			sumTax += (Math.min(grossIncome, 1350000) - 937901) * 0.166;
+		}
+		
+		if (grossIncome > 1350000) {
+			// Bracket E: income above 1,350,001 with a 17;6% tax
+			sumTax += (grossIncome - 1350001) * 0.176;
+		}
+		
+//		Display total taxes from input using JOptionPane
+		JOptionPane.showMessageDialog(null, "you will pay NOK " + sumTax + " in taxes", "Tax calculation", JOptionPane.INFORMATION_MESSAGE);
 	}
-
 }
--- src/no/hvl/dat100/O3.java
+++ src/no/hvl/dat100/O3.java
@@ -6,6 +6,31 @@ public class O3 {
 
 	public static void main(String[] args) {
 		
-//		
+		int fact = 1;
+		int n = 0;
+		
+//		Check if input is valid
+		while (true) {
+			
+//		Input a value of n from user using JOptionPane
+			String input = JOptionPane.showInputDialog("Enter an integer n");
+			
+//		Parse input n into an integer
+			n = Integer.parseInt(input);
+			
+			if (n > 0) { 
+				break; //break from while loop if input is valid
+				
+			} else {
+				JOptionPane.showMessageDialog(null, "Please enter a whole number greater than 0", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+				
+		}
+//		Use for loop to calculate factorial
+		for (int i = 1; i <= n; i++) {
+			fact *= i;
+		}
+		
+		JOptionPane.showMessageDialog(null, "The value of " + n + "! = " + fact, "Factorial result of n", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
