bf0e18928193a535f331f7b2f79fcef1e40680ec
h186487 <nadia.lambrecht@gmail.com>
2024-09-13 13:07:53 +0200

    O1 and O2 update
    O1 and O2 were incorrectly titled

--- src/no/hvl/dat100/O1.java
+++ src/no/hvl/dat100/O1.java
@@ -6,54 +6,42 @@ public class O1 {
 
 	public static void main(String[] args) {
 		
-//		Loop through 10 students
-		for (int i = 0; i < 10; i++) {
-//			Define student number as variable
-			int studentNumber = i + 1;
-			
-			int score;
-			
-			while (true) {
-//				Get input from user
-				String input = JOptionPane.showInputDialog("Enter the score for student " + studentNumber + ":");
-				
-				score = Integer.parseInt(input);
-				
-//				Check if input is valid
-				if (score >= 0 && score <= 100) {
-					break; //Exit for loop if input is valid
-//				Show an error message dialog box if input is invalid
-				} else { 
-					JOptionPane.showMessageDialog(null, "Invalid score entered. Please enter a score between 0 and 100.", "Error", JOptionPane.ERROR_MESSAGE);
-				}
-			}
-			
-			
-//			Determine grade scores which determines grades	
-			String Grade;
-				
-			if (score >= 90 && score <=100) {
-				Grade = "A";
-					
-			} else if (score >=80 && score <= 89) {
-				Grade = "B";
-				
-			} else if (score >=60 && score <= 79) {
-				Grade = "C";
-					
-			} else if (score >=50 && score <=59) {
-				Grade = "D";
-					
-			} else if (score >=40 && score <=49) {
-				Grade = "E";
-					
-			} else {
-				Grade = "F";
-					
-			}
-//			Print student number with their entered score and corresponding grade	
-			JOptionPane.showMessageDialog(null, "Student " + studentNumber + ": Score = " + score + ", Grade = " + Grade, "Grade", JOptionPane.INFORMATION_MESSAGE);
-				
-		}		
+//		Input gross income from user using JOptionPane
+		String input = JOptionPane.showInputDialog("Enter your gross income");
+//		Parse the input string into an integer
+		int grossIncome = Integer.parseInt(input);
+		
+//		Define variable
+		
+		double sumTax = 0.0;
+		
+//		"""Calculate tax for each bracket"""
+		if (grossIncome > 208050) {
+			//Bracket A: income between 208,051 - 292,850 with a 1,7% tax
+			sumTax += (Math.min(grossIncome, 292850) - 208051) * 0.017;
+		}
+		
+		if (grossIncome > 292850) {
+			//Bracket B: income between 292,851 - 670,000 with a 4,0% tax
+			sumTax += (Math.min(grossIncome,  670000) - 292851) * 0.04; 
+		}
+		
+		if (grossIncome > 670000) {
+			//Bracket C: income between 670,001 - 937,900 with a 13,6% tax
+			sumTax += (Math.min(grossIncome,  937900) - 670001) * 0.136;
+		}
+		
+		if (grossIncome > 937900) {
+			//Bracket D: income between 937,901 - 1,350,000 with a 16,6% tax
+			sumTax += (Math.min(grossIncome, 1350000) - 937901) * 0.166;
+		}
+		
+		if (grossIncome > 1350000) {
+			// Bracket E: income above 1,350,001 with a 17;6% tax
+			sumTax += (grossIncome - 1350001) * 0.176;
+		}
+		
+//		Display total taxes from input using JOptionPane
+		JOptionPane.showMessageDialog(null, "you will pay NOK " + sumTax + " in taxes", "Tax calculation", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
